4020cbdc0acbc2ec9e83c9d218f49ff6d5cd1404
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-09-17 01:47:46 +0100

    Remove hardcoded route

--- test/controllers/ApplicationSpec.scala
+++ test/controllers/ApplicationSpec.scala
@@ -1,12 +1,13 @@
 import org.scalatest.{FunSpec, Matchers}
 import play.api.test._
 import play.api.test.Helpers._
+import controllers.routes._
 
 class ApplicationSpec extends FunSpec with Matchers {
-  describe("Application Controller"){
-    it("should return a 200"){
+  describe("Application Controller") {
+    it("index route should return a status code 200"){
       running(FakeApplication()) {
-        val index = route(FakeRequest(GET, "/")).get
+        val index = route(FakeRequest(GET, Application.index.toString)).get
         status(index) should be (OK)
       }
     }
